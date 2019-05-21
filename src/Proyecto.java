08838cd0d300ca722f50b7a595d34692adfb5d64
Nicolas <pereznicolas90@gmail.com>
2019-05-21 11:18:57 -0500

    Parte 1 de los datos

--- src/Proyecto.java
+++ src/Proyecto.java
@@ -11,23 +11,23 @@ import java.awt.*;
 public class Proyecto { 
 	final static String esc = "\033[";
 	public static void main(String[] args) throws IOException, InterruptedException, SQLException, ClassNotFoundException { 
-	Accesos Acces=new Accesos();
-	JFrame frame = new JFrame("Exiting");
-	boolean salio=false;
-	while(salio==false) {
-	int accion=Acces.Ingreso(false);
-	if(accion!=-1) {
-	}
-	else {
-	salio=true;
-	JOptionPane.showMessageDialog(frame,
-	        "Adios",
-	        "   Exiting",
-	        JOptionPane.INFORMATION_MESSAGE);
-	}
-	System.out.println();
-	}
-	frame.dispose();
-	Entradas.reader.close();
+		Accesos Acces=new Accesos();
+		JFrame frame = new JFrame("Exiting");
+		boolean salio=false;
+		while(salio==false) {
+			int accion=Acces.Ingreso(false);
+			if(accion!=-1) {
+			}
+			else {
+				salio=true;
+				JOptionPane.showMessageDialog(frame,
+						"Adios",
+						"   Exiting",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+			System.out.println();
+		}
+		frame.dispose();
+		Entradas.reader.close();
 	}
 }
