af4ed65669bc541d36ce932d43a3d474bb21c7eb
Sabeto051 <bedoya.santi@gmail.com>
2019-05-21 08:07:49 -0500

    Simulacion casi completa sin bd

--- src/Simulacion.java
+++ src/Simulacion.java
@@ -35,6 +35,8 @@ public class Simulacion {
 		// Crea un nuevo usuario con Plan (porque es obligatiorio)
 		this.telefono = this.input.leerInt("Telefono", 0, 1000000000);
 		String pwd = this.input.leerString("Contrasena");
+		// Se muestran los planes
+		this.input.leerString("Numero del plan");
 		mostrarPlanUser();
 	}
 	public void loginUser() {
@@ -50,7 +52,7 @@ public class Simulacion {
 		// Se conecta la tabla Usuarios-Planes
 		System.out.println("Hola INSERTE NOMBRE AQUI");
 		System.out.println("Su plan acaba el INSERTE FECHA FINAL AQUI");
-		System.out.println("1. Comprar nuevo Plan\n2. Acceder a Escuelas\n Ver Cursos completados");
+		System.out.println("1. Comprar nuevo Plan\n2. Acceder a Escuelas\n3. Ver Cursos completados");
 		int option = this.input.leerInt("opcion deseada", 1, 3);
 		switch (option) {
 		case 1:
@@ -68,6 +70,7 @@ public class Simulacion {
 	public void mostrarCursosCompletados() {
 		// Se conecta con tabla Cursos , Usuarios y  Estudaintes-Cursos
 		// Se muestran los cursos completados
+		this.input.leerString("Cualquier tecla para retroceder\n\n\n");
 		mostrarPlanUser();
 	}
 	public void crearPlan() {
@@ -76,6 +79,7 @@ public class Simulacion {
 
 		// Se muestran planes disponibles
 		// Se escoge uno de los planes disponibles
+		this.input.leerString("Cualquier tecla para escoger plan\n\n\n");
 		mostrarPlanUser();
 	}
 	public void mostrarEscuelas() {
@@ -84,6 +88,7 @@ public class Simulacion {
 		
 		// se muestran las escuelas
 		// se escoge una escuela
+		this.input.leerString("Cualquier tecla para escoger escuela\n\n\n");
 		mostrarCarreras(); // de la escuela
 	}
 
@@ -91,34 +96,52 @@ public class Simulacion {
 		// se muestran las carreras de la escuela
 		System.out.println("Escuela INSERTE NOMBRE AQUI");
 		// se escoge una carrera
+		this.input.leerString("Cualquier tecla para escoger Carrera\n\n\n");
 		mostrarCursos(); // de la carrera
 	}
 	public void mostrarCursos() {
 		// se conecta con tabla cursos
 		System.out.println("Carrera INSERTE NOMBRE AQUI");
 		// se muestran los cursos y se escoge uno
+		this.input.leerString("Cualquier tecla para escoger Curso\n\n\n");
 		mostrarCursoMenu(); // del seleccionado
 	}
+	
+	
 	
 	public void mostrarCursoMenu() {
 		System.out.println("Curso INSERTE NOMBRE AQUI");
 		System.out.println("Profesor INSERTE NOMBRE AQUI");
-		System.out.println("1. Acceder a los foros del curso\n2. Acceder a los xvideos");
+		System.out.println("1. Acceder a los foros del curso\n2. Acceder a los videos");
 		int option = this.input.leerInt("opcion deseada", 1, 2);
 		switch (option) {
 		case 1:
-			mostrarVideos();
+			mostrarForos();
 			break;
 		case 2:
-			mostrarForos();
+			mostrarVideos();
 			break;
 		}
 	}
 	
 	public void mostrarVideos() {
-	
+		System.out.println("Curso INSERTE NOMBRE AQUI");
+		// Se muestra la lista de videos del curso
+		// Si se puede, mostrar en una columna al lado de los videos cuales ha visto
+		this.input.leerString("Cualquier tecla para ver Video\n\n\n");
+		verVideo();
+	}
+	public void verVideo() {
+		// se conecta la tabla videos y Estudiante-videos
+		// se agrega el video a videos vistos por el estudiante
+		System.out.println("Video visto");
+		this.input.leerString("Cualquier tecla para retroceder Curso\n\n\n");
+		mostrarVideos();
 	}
 	public void mostrarForos() {
+		// se conecta con la tabla foros
+		// se muestran los foros del curso
+		this.input.leerString("Cualquier tecla para escoger Foro\n\n\n");
 		
 	}
 
