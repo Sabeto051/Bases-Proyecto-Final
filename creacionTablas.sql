3fad26e97d8b864b5415e59006790a1c5ac10f37
APS96 <32958004+APS96@users.noreply.github.com>
2019-05-25 16:53:22 -0500

    sd
    sdf

--- creacionTablas.sql
+++ creacionTablas.sql
@@ -6,14 +6,14 @@ use Nisand;
 -- crear las tablas 
 create table if not exists Usuarios(id int auto_increment, telefono varchar(10), pwd varchar(20), primary key(id))ENGINE=INNODB;
 create table if not exists Planes(id int auto_increment, duracion varchar(10), valor int, primary key(id))ENGINE=INNODB;
-create table if not exists Escuelas(id int auto_increment, descripcion text, nombre varchar(20), primary key(id))ENGINE=INNODB;
+create table if not exists Escuelas(id int auto_increment, nombre varchar(20),descripcion text, primary key(id))ENGINE=INNODB;
 create table if not exists Profesores(id int auto_increment, telefono varchar(10), pwd varchar(20), salario int, primary key(id))ENGINE=INNODB;
 create table if not exists UsuariosPlanes(usuario_id int, fecha_inicio datetime, fecha_fin datetime, plan_id int, primary key(usuario_id), index p_id (plan_id), foreign key (plan_id) references Planes(id) on update cascade on delete cascade  )ENGINE=INNODB;
 create table if not exists PlanesEscuela(plan_id int, escuela_id int, primary key(plan_id, escuela_id))ENGINE=INNODB;
-create table if not exists Carreras(id int auto_increment, descripcion text, nombre varchar(30), escuela_id int, primary key (id), index e_id (escuela_id), foreign key (escuela_id) references Escuelas(id) on update cascade on delete cascade) ENGINE=INNODB;
-create table if not exists Cursos(id int auto_increment, descripcion text, nombre varchar(20), carrera_id int, profesor_id int, primary key (id), index c_id (carrera_id), index p_id (profesor_id), foreign key(carrera_id) references Carreras(id) on update cascade on delete cascade, foreign key(profesor_id) references Profesores(id) on update cascade on delete cascade);
+create table if not exists Carreras(id int auto_increment, nombre varchar(30), descripcion text,escuela_id int, primary key (id), index e_id (escuela_id), foreign key (escuela_id) references Escuelas(id) on update cascade on delete cascade) ENGINE=INNODB;
+create table if not exists Cursos(id int auto_increment, nombre varchar(20), descripcion text, carrera_id int, profesor_id int, primary key (id), index c_id (carrera_id), index p_id (profesor_id), foreign key(carrera_id) references Carreras(id) on update cascade on delete cascade, foreign key(profesor_id) references Profesores(id) on update cascade on delete cascade);
 create table if not exists Foros(id int auto_increment, nombre varchar(20), curso_id int, index c_id (curso_id), primary key (id), foreign key (curso_id) references Cursos(id) on update cascade on delete cascade) ENGINE=INNODB;
-create table if not exists Videos(id int auto_increment, curso_id int, duracion float, descripcion text, nombre varchar(20), primary key(id), index c_id (curso_id), foreign key(curso_id) references Cursos(id) on update cascade on delete cascade) ENGINE=INNODB;
+create table if not exists Videos(id int auto_increment, curso_id int, nombre varchar(20), duracion float, descripcion text, primary key(id), index c_id (curso_id), foreign key(curso_id) references Cursos(id) on update cascade on delete cascade) ENGINE=INNODB;
 create table if not exists Preguntas(id int auto_increment, foro_id int, usuario_id int, contenido text, fecha datetime, primary key(id), index f_id(foro_id), index u_id(usuario_id), foreign key(foro_id) references Foros(id) on update cascade on delete cascade, foreign key (usuario_id) references Usuarios(id) on update cascade on delete cascade) ENGINE=INNODB;
 create table if not exists Respuestas(id int auto_increment, pregunta_id int, usuario_id int, contenido text, fecha datetime, puntuacion int, primary key(id), index p_id(pregunta_id), index u_id(usuario_id), foreign key(pregunta_id) references Preguntas(id) on update cascade on delete cascade, foreign key(usuario_id) references Usuarios(id) on update cascade on delete cascade) ENGINE=INNODB;
 create table if not exists Estados(id int auto_increment, estado_tipo varchar(10), usuario_id int, index u_id(usuario_id), primary key (id), foreign key (usuario_id) references UsuariosPlanes(usuario_id) on update cascade on delete cascade) ENGINE=INNODB;
@@ -48,6 +48,7 @@ insert into LastNameU (usuario_id, apellido_id) values ('3', '3');
 -- Escuelas
 insert into Escuelas (descripcion, nombre) values('Las mejores carreras son de aca', 'ingenierias');
 insert into Escuelas (descripcion, nombre) values('Los que curan los enfermos', 'areas de la salud');
+insert into Escuelas (descripcion, nombre) values('No hacen nada', 'administrativa');
 
 -- Carreras
 insert into Carreras (descripcion, nombre, escuela_id) values ('La mejor ingenieria','Ingenieria de Sistemas','1');
@@ -119,6 +120,9 @@ insert into PlanesEscuela(plan_id, escuela_id) values ('2', '3');
 insert into PlanesEscuela(plan_id, escuela_id) values ('3', '2');
 
 -- test
+
+
+
 
 
 
